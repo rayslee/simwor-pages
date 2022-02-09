e358303282523879f68830fb8c9612fb173f3fce
raysleeee <raysleeee@gmail.com>
2022-02-09 17:43:04 +0800

    init head component

---
+++ src/components/Head.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Simwor</h1>
+  <h2>Make this a simple world.</h2>
+  <button>Bookcase</button>
+  <input type="text" placeholder="Search" @keydown.enter="queryBooks">
+</template>
+
+<script>
+export default {
+  name: 'Book',
+  setup() {
+    function queryBooks(e) {
+      console.log(e.target.value)
+      e.srcElement.value = ''
+      e.srcElement.blur()
+    }
+
+    return { queryBooks }
+  }
+}
+</script>
